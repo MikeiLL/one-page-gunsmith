5723f586705e907e420e630f4cef74893a718b84
Mike iLL Kilmer <mike@mzoo.org>
2019-02-11 14:46:14 -0600

    Add robots.txt sitemap.xml.

--- sync.sh
+++ sync.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-rsync -avP site.* *.ico *.svg *.png browserconfig.xml index.html bundles fonts images gunsmith:public_html
+rsync -avP robots.txt sitemap.xml site.* *.ico *.svg *.png browserconfig.xml index.html bundles fonts images gunsmith:public_html
